47c62bc225350559d6b5762ec98f597e18b1cbfe
Alexander Heinisch <e11906270@student.tuwien.ac.at>
2022-09-20 20:15:13 +0200

    added pyright disable flag since pyright doesn't recognize sqlalchemy meta classes

--- backend/ticketeer/models.py
+++ backend/ticketeer/models.py
@@ -1,4 +1,5 @@
-from extensions import db
+from . import db
 
-class Tag(db.Model):
+class Tag(db.Model): # type: ignore  [pyright thinking Model is wrong here]
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255))  
