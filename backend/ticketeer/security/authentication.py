b055c9b70289658883baa4ab6ac1ab9b435887f0
AlexHeinisch <6390741+AlexHeinisch@users.noreply.github.com>
2022-09-25 18:39:15 +0200

    changed login to now also include the id in the token

--- backend/ticketeer/security/authentication.py
+++ backend/ticketeer/security/authentication.py
@@ -9,7 +9,7 @@ from ticketeer.error.custom_errors import AuthorizationError
 from ticketeer.service.user_service import UserService
 
 from ticketeer.dto.schemas import LoginRequestSchema
-from ticketeer.dto.dtos import LoginRequestDto
+from ticketeer.dto.dtos import LoginRequestDto, UserRole
 
 authentication = Blueprint('authentication', __name__)
 
@@ -17,19 +17,19 @@ authentication = Blueprint('authentication', __name__)
 @authentication.route('/login', methods=['GET', 'POST'])
 def login(service: UserService):
     obj: LoginRequestDto = LoginRequestSchema().load(request.get_json()) # type: ignore
-    user = service.get_user_by_id(obj.username)
+    user = service.get_user_by_name(obj.username)
     if user and service.verify_login(obj):
-        token = generate_token(user.username, user.role, current_app.config['SECRET'])
+        token = generate_token(user.id, user.username, user.role, current_app.config['SECRET'])
         return {'token' : token}
     raise AuthorizationError('could not authenticate')
 
-def generate_token(username, role, secret):
-    token = jwt.encode(
+def generate_token(id: int, username: str, role: UserRole, secret: str):
+    return jwt.encode(
             {
+                'id': id,
                 'username' : username,
                 'role': role.name,
                 'exp' : datetime.datetime.utcnow() + datetime.timedelta(minutes=45)
             }, 
             secret, 
             "HS256")
-    return token
