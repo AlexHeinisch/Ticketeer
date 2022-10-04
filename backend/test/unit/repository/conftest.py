1d1aa197e1e38794a5f40b01c330431dc732a9c5
AlexHeinisch <6390741+AlexHeinisch@users.noreply.github.com>
2022-10-04 13:11:05 +0200

    added unit tests for user insertion and deletion

--- backend/test/unit/repository/conftest.py
+++ backend/test/unit/repository/conftest.py
@@ -1,15 +1,17 @@
-from flask_sqlalchemy import SQLAlchemy
 import pytest
+from ticketeer.dto.dtos import UserRole
 
 from ticketeer.repository.impl.sqlalchemy_user_repository import SQLAlchemyUserRepository, User
+
 
 @pytest.fixture(scope='session')
 def sample_user():
     usr: User = User(
-        id=1, 
+        id = 1,
         username='Alex', 
         password_hash='hello123', 
         email='test@example.com',
+        role=UserRole.USER
     )
     return usr
 
--- backend/test/unit/repository/test_sqlalchemy_user_repository.py
+++ backend/test/unit/repository/test_sqlalchemy_user_repository.py
@@ -1,3 +1,4 @@
+from ticketeer.dto.dtos import UserRegisterRequestDto, UserRole
 from ticketeer.models import User
 from ticketeer.repository.impl.sqlalchemy_user_repository import SQLAlchemyUserRepository
 
@@ -18,3 +19,42 @@ def test_get_user_by_id(
     mock_sqlalchemy.session.execute.return_value.one.return_value = sample_user
     assert sample_user.to_dto() == user_repo.get_user_by_id(sample_user.id)
     mock_sqlalchemy.select.return_value.filter_by.assert_called_once_with(id=sample_user.id)
+
+def test_insert_user(
+    sample_user: User,
+    user_repo: SQLAlchemyUserRepository,
+    mock_sqlalchemy
+    ):
+    user_repo.insert_user(UserRegisterRequestDto(username=sample_user.username,
+                                                 password=sample_user.password_hash,
+                                                 email=sample_user.email))
+    sample_user.id= -1
+    sample_user.role = UserRole.USER
+    mock_sqlalchemy.session.add.assert_called_once_with(sample_user)
+    mock_sqlalchemy.session.commit.assert_called_once()
+
+def test_delete_user_by_name(
+    sample_user: User,
+    user_repo: SQLAlchemyUserRepository,
+    mock_sqlalchemy
+    ):
+    mock_sqlalchemy.session.execute.return_value.one.return_value = sample_user
+    user_repo.delete_user_by_name(sample_user.username)
+
+    mock_sqlalchemy.select.return_value.filter_by.assert_called_once_with(username=sample_user.username)
+    mock_sqlalchemy.session.delete.assert_called_once_with(sample_user)
+    mock_sqlalchemy.session.commit.assert_called_once()
+
+
+def test_delete_user_by_id(
+    sample_user: User,
+    user_repo: SQLAlchemyUserRepository,
+    mock_sqlalchemy
+    ):
+    mock_sqlalchemy.session.execute.return_value.one.return_value = sample_user
+    user_repo.delete_user_by_id(sample_user.id)
+
+    mock_sqlalchemy.select.return_value.filter_by.assert_called_once_with(id=sample_user.id)
+    mock_sqlalchemy.session.delete.assert_called_once_with(sample_user)
+    mock_sqlalchemy.session.commit.assert_called_once()
+
