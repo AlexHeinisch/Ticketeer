94d7eb79fb60c18b3d369f0be04bf40beee9883c
AlexHeinisch <6390741+AlexHeinisch@users.noreply.github.com>
2022-10-06 11:57:59 +0200

    adjusted user endpoint to match new authorization scheme and be more layer-conform

--- backend/ticketeer/endpoint/user_endpoint.py
+++ backend/ticketeer/endpoint/user_endpoint.py
@@ -1,18 +1,18 @@
 import logging
 from werkzeug.exceptions import BadRequest
-from typing import cast
+from typing import Optional, cast
 from flask import Blueprint, jsonify, request
 
 # DI
-from injector import inject
+from injector import NoInject, inject
 from ticketeer.service.user_service import UserService
 
 # Validation
 from ticketeer.dto.schemas import UserRegisterRequestSchema, UserSchema, UserSearchRequestSchema, UserUpdateRequestSchema
-from ticketeer.dto.dtos import UserRegisterRequestDto, UserUpdateRequestDto, UserSearchRequestDto, UserSearchRequestDto
+from ticketeer.dto.dtos import UserRegisterRequestDto, UserRole, UserUpdateRequestDto, UserSearchRequestDto, UserSearchRequestDto
 
 # Auth
-from ..security.authorization import jwt_required
+from ..security.authorization import CurrentPermissions, jwt_required
 
 user = Blueprint('user', __name__)
 logger = logging.getLogger(__name__)
@@ -20,18 +20,18 @@ logger = logging.getLogger(__name__)
 
 @inject
 @user.route('/<id>', methods=['GET'])
-#@jwt_required()
+@jwt_required()
 def get_user_by_id(service: UserService, id: int):
     logger.info(f'[GET] single user: id={id}')
     return UserSchema().dump(service.get_user_by_id(id))
 
 @inject
 @user.route('', methods=['GET'])
-#@jwt_required()
+@jwt_required()
 def get_users_by_search(service: UserService):
     req: UserSearchRequestDto = cast(UserSearchRequestDto, UserSearchRequestSchema().load(request.args))
     logger.info(f'[GET] users: search-req={req}')
-    return jsonify(UserSchema(many=True).dump(service.get_multiple_users(req)))
+    return UserSchema(many=True).dump(service.get_multiple_users(req))
 
 @inject
 @user.route('', methods=['POST'])
@@ -44,18 +44,18 @@ def post_user(service: UserService):
 
 @inject
 @user.route('/<id>', methods=['DELETE'])
-#@jwt_required()
-def delete_user(service: UserService, id: int):
+@jwt_required(UserRole.ADMIN, "BEARER[id] == <id>")
+def delete_user(service: UserService, id: int, perm: NoInject[CurrentPermissions]):
     service.delete_user_by_id(id)
     return '', 204
 
 @inject
-@user.route('/<name>', methods=['PATCH'])
-#@jwt_required()
-def patch_user(service: UserService, name: str):
+@user.route('/<id>', methods=['PATCH'])
+@jwt_required(UserRole.ADMIN, "BEARER[id] == <id>")
+def patch_user(service: UserService, id: int, perm: NoInject[CurrentPermissions]):
     body = request.get_json()
     if not body:
         raise BadRequest()
     req : UserUpdateRequestDto = cast(UserUpdateRequestDto, UserUpdateRequestSchema().load(body))
-    req.username = name
-    return UserSchema().dump(service.update_user(req))
+    req.id = id
+    return UserSchema().dump(service.update_user(req, perm))
