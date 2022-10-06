23893d69c48ab3ac3c5b42f42a1f4ef370257777
AlexHeinisch <6390741+AlexHeinisch@users.noreply.github.com>
2022-10-06 11:54:58 +0200

    refactored fetching of json-body-data and adjusted expiration of tokens from 45min to 14 days

--- backend/ticketeer/security/authentication.py
+++ backend/ticketeer/security/authentication.py
@@ -1,6 +1,8 @@
 from flask import Blueprint, jsonify, request, current_app
 import datetime
 import jwt
+import werkzeug.exceptions
+from typing import cast
 
 from injector import inject
 
@@ -16,11 +18,17 @@ authentication = Blueprint('authentication', __name__)
 @inject
 @authentication.route('/login', methods=['GET', 'POST'])
 def login(service: UserService):
-    obj: LoginRequestDto = LoginRequestSchema().load(request.get_json()) # type: ignore
-    user = service.get_user_by_name(obj.username)
-    if user and service.verify_login(obj):
-        token = generate_token(user.id, user.username, user.role, current_app.config['SECRET'])
-        return {'token' : token}
+    body = request.get_json()
+    if not body:
+        raise werkzeug.exceptions.BadRequest()
+    obj: LoginRequestDto = cast(LoginRequestDto, LoginRequestSchema().load(body))
+    try:
+        user = service.get_user_by_name(obj.username)
+        if user and service.verify_login(obj):
+            token = generate_token(user.id, user.username, user.role, current_app.config['SECRET_KEY'])
+            return {'token' : token}
+    except:
+        ...
     raise AuthorizationError('could not authenticate')
 
 def generate_token(id: int, username: str, role: UserRole, secret: str):
@@ -29,7 +37,7 @@ def generate_token(id: int, username: str, role: UserRole, secret: str):
                 'id': id,
                 'username' : username,
                 'role': role.name,
-                'exp' : datetime.datetime.utcnow() + datetime.timedelta(minutes=45)
+                'exp' : datetime.datetime.utcnow() + datetime.timedelta(days=14)
             }, 
             secret, 
             "HS256")
