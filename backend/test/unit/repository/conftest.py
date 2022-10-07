df652a91e4dd319aa3e30ee721c6846ed189da3b
AlexHeinisch <6390741+AlexHeinisch@users.noreply.github.com>
2022-10-07 13:04:03 +0200

    moved user to general unit test config and added proper password and hash generation

--- backend/test/unit/repository/conftest.py
+++ backend/test/unit/repository/conftest.py
@@ -1,19 +1,6 @@
 import pytest
-from ticketeer.dto.dtos import UserRole
 
-from ticketeer.repository.impl.sqlalchemy_user_repository import SQLAlchemyUserRepository, User
-
-
-@pytest.fixture(scope='session')
-def sample_user():
-    usr: User = User(
-        id = 1,
-        username='Alex', 
-        password_hash='hello123', 
-        email='test@example.com',
-        role=UserRole.USER
-    )
-    return usr
+from ticketeer.repository.impl.sqlalchemy_user_repository import SQLAlchemyUserRepository
 
 @pytest.fixture(scope='function')
 def mock_sqlalchemy(mocker):
