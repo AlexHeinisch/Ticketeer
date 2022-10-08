31c7ebf1cdabc24d8e20c43e730e4ee5552e68c2
AlexHeinisch <6390741+AlexHeinisch@users.noreply.github.com>
2022-10-08 19:30:09 +0200

    removed multi-layer functionality because it would be too much redundant work for now

--- backend/test/integration/test_users.py
+++ backend/test/integration/test_users.py
@@ -1,12 +1,4 @@
-import pytest
 
-@pytest.mark.parametrize('client_name',[
-                             pytest.param('level1_client', marks=pytest.mark.dependency(name='l1_test_1')),
-                             pytest.param('level2_client', marks=pytest.mark.dependency(name='l2_test_1', depends=['l1_test_1'])),
-                             pytest.param('level3_client', marks=pytest.mark.dependency(name='l3_test_1', depends=['l2_test_1'])),
-                             pytest.param('level4_client', marks=pytest.mark.dependency(name='l4_test_1', depends=['l3_test_1'])),
-                         ])
-def test_1_get_users_no_authorization_token(client_name, request):
-    client = request.getfixturevalue(client_name)
+def test_get_users_no_authorization_token(client):
     response = client.get('/user')
     assert b'no authorization token provided' in response.data
