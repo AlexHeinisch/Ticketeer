598f651f81e6551c559f9e344d731a6c994409d8
AlexHeinisch <6390741+AlexHeinisch@users.noreply.github.com>
2022-10-08 19:36:54 +0200

    moved sample users to root test directory to also use these users in integration tests, also added more users!

---
+++ backend/test/conftest.py
@@ -0,0 +1,61 @@
+import pytest
+from ticketeer.dto.dtos import UserRole
+from werkzeug.security import generate_password_hash
+from ticketeer.models import User
+
+@pytest.fixture(scope='session')
+def sample_user_password():
+    return 'hello123'
+
+@pytest.fixture(scope='session')
+def sample_user_password_hash(sample_user_password):
+    return generate_password_hash(sample_user_password)
+
+@pytest.fixture(scope='function')
+def sample_user(sample_user_password_hash):
+    usr: User = User(
+        id = 1,
+        username='Alex', 
+        password_hash=sample_user_password_hash,
+        email='test@example.com',
+        role=UserRole.USER
+    )
+    return usr
+
+@pytest.fixture(scope='session')
+def sample_user2_password():
+    return 'hello12345'
+
+@pytest.fixture(scope='session')
+def sample_user2_password_hash(sample_user2_password):
+    return generate_password_hash(sample_user2_password)
+
+@pytest.fixture(scope='function')
+def sample_user2(sample_user2_password_hash):
+    usr: User = User(
+        id = 3,
+        username='Alexandria', 
+        password_hash=sample_user2_password_hash,
+        email='alexa@example.com',
+        role=UserRole.USER
+    )
+    return usr
+
+@pytest.fixture(scope='session')
+def sample_admin_password():
+    return 'somepass123'
+
+@pytest.fixture(scope='session')
+def sample_admin_password_hash(sample_admin_password):
+    return generate_password_hash(sample_admin_password)
+
+@pytest.fixture(scope='function')
+def sample_admin(sample_admin_password_hash):
+    usr: User = User(
+        id = 2,
+        username='Oleg', 
+        password_hash=sample_admin_password_hash,
+        email='admin@example.com',
+        role=UserRole.ADMIN
+    )
+    return usr
